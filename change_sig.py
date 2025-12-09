9a61f3f7166f7ac8b5d8674ecd450fbb063ed2bb
JernoBeuker0 <jernobeuker0@gmail.com>
2025-12-09 15:55:13 +0100

    Adding better prints

--- change_sig.py
+++ change_sig.py
@@ -21,19 +21,27 @@ def single_cr2_to_jpg(input_path, output_path, quality=95):
         quality=quality,
     )
 
-    print(f"Saved JPEG to: {output_path}")
-
 
 def main():
     os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+    total = len(os.listdir(INPUT_FOLDER))
+    count = 0
 
     for file in os.listdir(INPUT_FOLDER):
         if file.lower().endswith(".cr2"):
             input_path = os.path.join(INPUT_FOLDER, file)
             output_file = os.path.splitext(file)[0] + ".jpg"
             output_path = os.path.join(OUTPUT_FOLDER, output_file)
+            
+            
+            count += 1
+            if count % 10 == 0:
+                print(f"Processed {count}/{total}")
 
             single_cr2_to_jpg(input_path, output_path)
+
+    print("Done!!!")
 
 
 if __name__ == "__main__":
